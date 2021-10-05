87ba26440cd1254dddc7ce8ead5ea65b361bebb0
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-05 13:47:17 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/linemethod/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bdlz.linemethod;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the LineComparison Program");
+    }
+}
